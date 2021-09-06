e62c800fc880f9cede410ef96576b3105d5f5bd4
Bhargav-T <74995430+Bhargav-T@users.noreply.github.com>
2021-09-06 07:46:58 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+var canvas = new fabric.Canvas('myCanvas');
+
+var player_x = 10;
+var player_y = 10;
+
+var block_image_width = 30;
+var block_image_height = 30;
+
+var player_object = "";
+var block_image_object = "";
+
+function player_update(){
+    fabric.Image.fromURL("Screenshot 2021-09-06 074042");
+}
